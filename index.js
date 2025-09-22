1d2da7a4e9ee7f01b59ea854f95d79e4db02819c
burlak87 <burlakovfedor87@gmail.com>
2025-09-22 09:42:54 +0300

    burlak87

---
+++ index.js
@@ -0,0 +1,41 @@
+import jsonfile from 'jsonfile'
+import moment from 'moment'
+import simpleGit from 'simple-git'
+
+const path = './data.json'
+const git = simpleGit()
+
+const makeCommits = n => {
+	if (n === 0) return git.push()
+	const x = Math.floor(Math.random() * 55)
+	const y = Math.floor(Math.random() * 7)
+	console.log(x, y)
+
+	const hour = Math.floor(Math.random() * 25)
+	const minute = Math.floor(Math.random() * 61)
+	const seconds = Math.floor(Math.random() * 61)
+	const time = { hour: hour, minute: minute, second: seconds }
+	console.log(time)
+
+	const date = moment()
+		.subtract(1, 'y')
+		.add(1, 'd')
+		.add(x, 'w')
+		.add(y, 'd')
+		.set(time)
+		.format()
+
+	const data = {
+		date: date,
+	}
+	console.log(date)
+
+	jsonfile.writeFile(path, data, () => {
+		git
+			.add([path])
+			.commit(date, { '--date': date }, makeCommits.bind(this, --n))
+			.push()
+	})
+}
+
+makeCommits(10)
