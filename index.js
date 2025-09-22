edfea4acdd6f8d0ddfed445e74d90fbcfdd48be9
burlak87 <burlakovfedor87@gmail.com>
2025-09-23 00:47:37 +0300

    sdfdgf

--- index.js
+++ index.js
@@ -2,48 +2,74 @@ import jsonfile from 'jsonfile'
 import moment from 'moment'
 import simpleGit from 'simple-git'
 
-const path = './data.json'
+const path = [
+	'./dataStorage/data1.json',
+	'./dataStorage/data2.json',
+	'./dataStorage/data3.json',
+	'./dataStorage/data4.json',
+	'./dataStorage/data5.json',
+]
+
 const git = simpleGit()
 
 const makeCommits = n => {
 	if (n === 0) return git.push()
-	const x = Math.floor(Math.random() * 55)
-	const y = Math.floor(Math.random() * 7)
-	console.log(x, y)
-
-	commits(x, y, n)
+	for (let i = n; i > 0; i--) {
+		const dataCommits = getData()
+		getCommits(dataCommits, n)
+	}
 }
 
-async function commits(x, y, n) {
-	for (let i = 0; i < 3; i++) {
-		if (n===0) return git.push()
-		const hour = Math.floor(Math.random() * 25)
-		const minute = Math.floor(Math.random() * 61)
-		const seconds = Math.floor(Math.random() * 61)
-		const time = { hour: hour, minute: minute, second: seconds }
-		console.log(time)
+function getData() {
+	let data = []
 
-		const date = moment()
-			.subtract(1, 'y')
-			.add(1, 'd')
-			.add(x, 'w')
-			.add(y, 'd')
-			.set(time)
-			.format()
+	for (let i = 0; i < 5; i++) {
+		const x = Math.floor(Math.random() * 55)
+		const y = Math.floor(Math.random() * 7)
 
-		const data = {
-			date: date,
+		let time = []
+		for (let j = 0; j < 5; j++) {
+			const hour = Math.floor(Math.random() * 25)
+			const minute = Math.floor(Math.random() * 25)
+			const second = Math.floor(Math.random() * 25)
+			time.push({ hour: hour, minute: minute, second: second })
 		}
-		console.log(date)
 
-		await new Promise((resolve, reject) => {
-			jsonfile.writeFile(path, data, (err) => {
-				if (err) reject(err)
-				else resolve()
+		data[i] = {
+			x: x,
+			y: y,
+			time: time
+		}
+	}
+
+	console.log(data)
+	return data
+}
+
+function getCommits(dataCommits, n) {
+	for (let i = 0; i < 5; i++) {
+		const timeCommit = dataCommits[i].time
+		for (let j = 0; i < 5; i++) {
+			const date = moment()
+				.subtract(1, 'y')
+				.add(1, 'd')
+				.add(dataCommits[i].x, 'w')
+				.add(dataCommits[i].y, 'd')
+				.set(timeCommit[j])
+				.format()
+
+			const data = {
+				date: date,
+			}
+			console.log(date)
+
+			jsonfile.writeFile(path[i], data, () => {
+				git
+					.add([path[i]])
+					.commit(date, { '--date': date }, makeCommits.bind(this, --n))
+					.push()
 			})
-		})
-
-		await git.add([path]).commit(date, {'--date':date}, makeCommits.bind(this, --n)).push()
+		}
 	}
 }
 
