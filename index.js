ec255638a917536e30a8daa525b5d3f6a28c768b
burlak87 <burlakovfedor87@gmail.com>
2025-09-22 19:48:55 +0300

    ad

--- index.js
+++ index.js
@@ -11,31 +11,40 @@ const makeCommits = n => {
 	const y = Math.floor(Math.random() * 7)
 	console.log(x, y)
 
-	const hour = Math.floor(Math.random() * 25)
-	const minute = Math.floor(Math.random() * 61)
-	const seconds = Math.floor(Math.random() * 61)
-	const time = { hour: hour, minute: minute, second: seconds }
-	console.log(time)
-
-	const date = moment()
-		.subtract(1, 'y')
-		.add(1, 'd')
-		.add(x, 'w')
-		.add(y, 'd')
-		.set(time)
-		.format()
-
-	const data = {
-		date: date,
-	}
-	console.log(date)
-
-	jsonfile.writeFile(path, data, () => {
-		git
-			.add([path])
-			.commit(date, { '--date': date }, makeCommits.bind(this, --n))
-			.push()
-	})
+	commits(x, y, n)
 }
 
-makeCommits(10)
+async function commits(x, y, n) {
+	for (let i = 0; i < 3; i++) {
+		if (n===0) return git.push()
+		const hour = Math.floor(Math.random() * 25)
+		const minute = Math.floor(Math.random() * 61)
+		const seconds = Math.floor(Math.random() * 61)
+		const time = { hour: hour, minute: minute, second: seconds }
+		console.log(time)
+
+		const date = moment()
+			.subtract(1, 'y')
+			.add(1, 'd')
+			.add(x, 'w')
+			.add(y, 'd')
+			.set(time)
+			.format()
+
+		const data = {
+			date: date,
+		}
+		console.log(date)
+
+		await new Promise((resolve, reject) => {
+			jsonfile.writeFile(path, data, (err) => {
+				if (err) reject(err)
+				else resolve()
+			})
+		})
+
+		await git.add([path]).commit(date, {'--date':date}, makeCommits.bind(this, --n)).push()
+	}
+}
+
+makeCommits(1)
